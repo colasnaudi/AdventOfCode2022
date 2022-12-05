b73a22592df882898353643b47f77906011c45b6
Colas NAUDI <colasnaudi@gmail.com>
2022-12-05 13:41:27 +0100

    commit

--- Day3/Exercise1/main.py
+++ Day3/Exercise1/main.py
@@ -1,8 +1,38 @@
+from string import ascii_lowercase
+LETTERS = {letter: str(index) for index, letter in enumerate(ascii_lowercase, start=1)} 
+
+def alphabet_position(text):
+    text = text.lower()
+    numbers = [LETTERS[character] for character in text if character in LETTERS]
+    return ' '.join(numbers)
+
 def main():
-    print("Hello World")
+    somme = 0
+    with open('Day3/test/input.txt', 'r') as file:
+        file = file.read()
+        file = file.splitlines()
+        file = [i.split() for i in file]
+        for i in file:
+            l = len(i[0])
+            print ("First : ",i[0][:l//2])
+            print ("Second : ",i[0][l//2:])
+            for k in i[0][:l//2]:
+                if k in (i[0][l//2:]):
+                    letter = k
+                    break
+            print ("The letter is ",letter)
+
+            if(letter.islower()):
+                print(alphabet_position(letter))
+                somme += int(alphabet_position(letter))
+            else:
+                print(int((alphabet_position(letter)))+26)
+                somme += int((alphabet_position(letter)))+26
+
+    print("La somme totale est : ",somme)
 
 if __name__ == "__main__":
     main()
 
-# The test result is 
-# The input result is 
+# The test result is 157
+# The input result is 7568
--- Day3/Exercise2/main.py
+++ Day3/Exercise2/main.py
@@ -1,8 +1,39 @@
+from string import ascii_lowercase
+LETTERS = {letter: str(index) for index, letter in enumerate(ascii_lowercase, start=1)} 
+
+def alphabet_position(text):
+    text = text.lower()
+    numbers = [LETTERS[character] for character in text if character in LETTERS]
+    return ' '.join(numbers)
+
 def main():
-    print("Hello World")
+    somme = 0
+    file = open('Day3/test/input.txt', "r")
+    line = file.readline()
+    while line:
+        line2 = file.readline()
+        line3 = file.readline()
+        for i in line:
+            if i in line2 and i in line3:
+                letter = i
+                break
+        line = file.readline()
+
+        print ("The common letter is ",letter)
+
+        if(letter.islower()):
+            print("It worth : ",alphabet_position(letter))
+            somme += int(alphabet_position(letter))
+        else:
+            print("It worth : ",int((alphabet_position(letter)))+26)
+            somme += int((alphabet_position(letter)))+26
+
+    file.close()
+
+    print("La somme totale est : ",somme)
 
 if __name__ == "__main__":
     main()
 
-# The test result is 
-# The input result is 
+# The test result is 70
+# The input result is 2780
